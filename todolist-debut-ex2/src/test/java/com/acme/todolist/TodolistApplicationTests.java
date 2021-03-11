542911b7904d77302cc16cf2198162b556237ee3
tp <kevchanut@gmail.com>
2021-03-11 19:42:41 +0100

    fin exercice 3, Ajout du test et ajout du read me

--- todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplicationTests.java
+++ todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplicationTests.java
@@ -1,13 +1,26 @@
 package com.acme.todolist;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.acme.todolist.configuration.TodolistApplication;
+import com.acme.todolist.domain.TodoItem;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@SpringBootTest(classes = TodolistApplication.class)
 class TodolistApplicationTests {
 
+	
+	
 	@Test
-	void contextLoads() {
+	void IsLate() {
+		TodoItem todoItem = new TodoItem("1", Instant.now().minus(24, ChronoUnit.HOURS), "Body");
+		assertThat(todoItem.finalContent(), todoItem.isLate(),is(true));
 	}
 
 }
